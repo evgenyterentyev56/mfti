d7011ec2b67b0147b55f7d859009b5bbac997117
Evgeny <evgenyterentyev56@yandex.ru>
2020-05-10 22:25:19 +0400

    gggg

---
+++ test.py
@@ -0,0 +1,15 @@
+
+
+
+a = int(input(':')) # 1534
+
+# The next number for the number 0 is 1.
+# The previous number for the number 0 is -1.
+
+# Следующее число для числа 1534 - 1535.
+# Предыдущее число для числа 1534 - 1533.
+
+
+
+print('The next number for the number ' + str(a) + ' is' + str(a) + str(1) )
+# print(type(a))
